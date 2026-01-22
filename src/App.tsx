37aae1c888d8b6df6150d671a37c8d297c8b987f
FrontendSuberu <joeysuberu@gmail.com>
2026-01-22 02:48:55 +0100

    routing paths for diffrent pages created

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
+import { Route, Routes } from "react-router";
 import "./App.css";
+import Home from "./pages/Home/Home";
+import Layout from "./components/Layout/Layout";
+import About from "./pages/About/About";
+import Programs from "./pages/Programs/Programs";
+import Contact from "./pages/Contact/Contact";
+import Admissions from "./pages/Admissions/Admissions";
 
 function App() {
-
   return (
-    <>
-      <h1 className="text-3xl font-medium">Hello</h1>
-    </>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/programs" element={<Programs />} />
+        <Route path="/admissions" element={<Admissions />} />
+        <Route path="/contact" element={<Contact />} />
+      </Route>
+    </Routes>
   );
 }
 

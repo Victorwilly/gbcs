0e51e631c60e5b34b56219c10492c4cb3b1e4c2a
FrontendSuberu <joeysuberu@gmail.com>
2026-03-05 21:41:10 +0100

    added hook to reset scroll position on route change

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,8 +1,17 @@
 import Header from "../Header/Header";
 import { Outlet } from "react-router-dom";
 import Footer from "../Footer/Footer";
+import { useEffect } from "react";
+import { useLocation } from 'react-router-dom';
 
 export default function Layout() {
+
+    const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
   return (
     <>
       <Header />

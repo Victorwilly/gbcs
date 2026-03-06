a60d58f6414fa1a530e5c5a26f5d04d1ecf63137
FrontendSuberu <joeysuberu@gmail.com>
2026-03-06 14:20:56 +0100

    routes added for the app component

--- src/App.tsx
+++ src/App.tsx
@@ -7,10 +7,12 @@ import Programs from "./pages/Programs/Programs";
 import Contact from "./pages/Contact/Contact";
 import Admissions from "./pages/Admissions/Admissions";
 import Register from "./pages/Register/Register";
-
+import Login from "./pages/Login/Login";
+import LoginStudent from "./pages/Login/LoginStudent";
+import LoginLecturer from "./pages/Login/LoginLecturer";
+import LoginAdministrator from "./pages/Login/LoginAdmistrator";
 
 function App() {
-
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
@@ -20,6 +22,12 @@ function App() {
         <Route path="/admissions" element={<Admissions />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/register" element={<Register />} />
+
+        {/* login components */}
+        <Route path="/login" element={<Login />} />
+        <Route path="/login/administrator" element={<LoginAdministrator />} />
+        <Route path="/login/student" element={<LoginStudent />} />
+        <Route path="/login/lecturer" element={<LoginLecturer />} />
       </Route>
     </Routes>
   );

ced142f150f2b00c1f6e247c883c1f6e11eea68a
FrontendSuberu <joeysuberu@gmail.com>
2026-03-12 01:40:56 +0100

    code cleanup

--- src/components/Header/DashboardHeader.tsx
+++ src/components/Header/DashboardHeader.tsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 const DashboardHeader = () => {
   return (
-    <header className="flex justify-between border-b-[0.5px] border-[#C3C2C2] w-full items-center py-6 px-[14px] md:px-[32px] bg-white">
+    <header className="flex justify-between border-b-[0.5px] border-[#C3C2C2] w-full items-center py-6 px-3.5 md:px-8 bg-white">
       <div className="flex flex-col">
         <h1 className="text-xl font-medium">Welcome back, John Doe!</h1>
         <p className="text-[#6B7280] pt-1 text-[12px]">
